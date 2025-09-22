127f9cd6817867acc5589b51fe8b5a529931507d
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2025-09-22 17:00:58 +0200

    Added helper functions to generate opening and closing times for each day. Added function to filter bookings to only get this weeks bookings.

---
+++ src/interfaces/OpeningHours.ts
@@ -0,0 +1,4 @@
+export default interface OperatingHours{
+    openingTime: Date
+    closingTime: Date
+}
--- src/pages/AvailableTimesPage.tsx
+++ src/pages/AvailableTimesPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Row, Col, Container, Card } from 'react-bootstrap';
 import type UnparsedBooking from '../interfaces/UnparsedBooking.ts';
 import type ParsedBooking from '../interfaces/ParsedBooking.ts';
+import type OperatingHours from '../interfaces/OpeningHours.ts';
 AvailableTimesPage.route = {
     path: '/available-times',
     menuLabel: 'Available timeslots',
@@ -18,12 +19,34 @@ export default function AvailableTimesPage() {
     // api/bookings?WHERE=startTime>2025-09-12 12:00:00
 
 
+    function generateDayOperatingTimes(date: Date): OperatingHours {
+        const startTime = new Date(date)
+        startTime.setHours(17, 0, 0)
+        const endTime = new Date(date)
+        endTime.setHours(23, 0, 0)
+        return { openingTime: startTime, closingTime: endTime }
+    }
+
+    // generate a weeks worth of operating hours for a given first day of week
+    function generateWeekOperatingTImes(monday: Date): OperatingHours[] {
+        const weekOperatingHours: OperatingHours[] = []
+
+        let dateChecked = new Date(monday);
+        for (let i = 1; i <= 7; i++) {
+            const dailyOperatingHours = generateDayOperatingTimes(dateChecked)
+            weekOperatingHours.push(dailyOperatingHours)
+            dateChecked.setDate(dateChecked.getDate() + 1)
+        }
+        return weekOperatingHours
+    }
+
+
     function parseBooking(item: UnparsedBooking): ParsedBooking {
         return {
             id: item.id,
             userId: item.userId,
-            startTime: new Date(item.startTime.replace(" ", "T")),
-            endTime: new Date(item.endTime.replace(" ", "T")),
+            startTime: new Date(item.startTime),
+            endTime: new Date(item.endTime),
             partySize: item.partySize
         }
     }
@@ -36,17 +59,7 @@ export default function AvailableTimesPage() {
 
     const bookableTime = new Date(today.getTime());
     bookableTime.setHours(bookableTime.getHours() + timeMarginHours);
-    const timeString = bookableTime.toLocaleString(locale)
-
-
-    // Fetches all booked times that starts after current time + 2 hours, sets it to bookings <Booking[]>.
-    useEffect(() => {
-        fetch(`/api/bookings?WHERE=startTime>${timeString}`)
-            .then((res) => res.json())
-            .then((data: UnparsedBooking[]) => {
-                setBookings(parseBookingList(data))
-            })
-    }, [])
+    const timeString = bookableTime.toISOString().split('.')[0]
 
     // -- Making weeks --
     const currentWeekday = today.getDay()
@@ -60,8 +73,23 @@ export default function AvailableTimesPage() {
         return date
     })
 
-    const currentWeek = false
+    // Fetches all booked times that starts after current time + 2 hours, sets it to bookings <Booking[]>.
+    useEffect(() => {
+        fetch(`/api/bookings?WHERE=startTime>${timeString}`)
+            .then((res) => res.json())
+            .then((data: UnparsedBooking[]) => {
+                setBookings(parseBookingList(data))
+            })
+    }, [])
 
+    function filterBookingsForWeek(monday: Date, allBookings: ParsedBooking[]): ParsedBooking[] {
+        monday.setHours(17, 0, 0)
+        const sunday = new Date(monday)
+        sunday.setDate(monday.getDate() + 6)
+        sunday.setHours(21, 0, 0)
+
+        return allBookings.filter((x) => x.startTime.toISOString() >= monday.toISOString() && x.startTime.toISOString() <= sunday.toISOString())
+    }
     return <>
         <Row>
             <Col>
